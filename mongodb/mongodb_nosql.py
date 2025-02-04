eb9fc53d542eefb6e99a6ddeb53635d3f7264fc4
quentbt <quentinbuet4@gmail.com>
2025-02-04 14:39:17 +0100

    crud mongodb

--- mongodb/mongodb_nosql.py
+++ mongodb/mongodb_nosql.py
@@ -8,3 +8,19 @@ collection = db.mycollection
 document = {"name": "John Doe", "email": "john.doe@example.com", "age": 30}
 result = collection.insert_one(document)
 print("Inserted document ID:", result.inserted_id)
+
+# Récupération
+query = {"name": "John Doe"}
+document = collection.find_one(query)
+print(document)
+
+# Modification
+query = {"name": "John Doe"}
+update = {"$set": {"age": 31}}
+result = collection.update_one(query, update)
+print("Modified document count:", result.modified_count)
+
+# suppression
+query = {"name": "John Doe"}
+result = collection.delete_one(query)
+print("Deleted document count:", result.deleted_count)
